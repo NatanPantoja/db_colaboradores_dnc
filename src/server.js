b2c237b2f96995e442d7e1e7505b9abb591a8c92
Natanael Pantoja <161660708+NatanPantoja@users.noreply.github.com>
2025-10-22 01:09:34 -0300

    Reorder express.json() middleware usage

--- src/server.js
+++ src/server.js
@@ -4,13 +4,13 @@ import cors from "cors";
 import router from "./router.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
-
-app.use(express.json());
 const corsOptions = {
   origin: "*", 
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type, Authorization", 
 };
+
+app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
 app.use(errorHandler);
