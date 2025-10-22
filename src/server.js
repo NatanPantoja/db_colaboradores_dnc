e837f0e9373dc5439c228bb97141665693c50e79
Natanael Pantoja <161660708+NatanPantoja@users.noreply.github.com>
2025-10-22 01:42:22 -0300

    Update allowedHeaders in CORS options

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false, 
   optionsSuccessStatus: 204, 
-  allowedHeaders: "Content-Type, Authorization",
+  allowedHeaders: "Content-Type, x-access-token",
 };
 
 app.use(cors(corsOptions));
