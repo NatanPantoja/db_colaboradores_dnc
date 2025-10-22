a3f9ebed077638471c9742d1e160935bc2bcd582
Natanael Pantoja <161660708+NatanPantoja@users.noreply.github.com>
2025-10-22 13:43:10 -0300

    Update AuthUserService for user role and table change
    Refactor AuthUserService to use 'colaborador' table and include user role in token.

--- src/service/user/AuthUserService.js
+++ src/service/user/AuthUserService.js
@@ -1,3 +1,4 @@
+// src/service/user/AuthUserService.js
 import prismaClient from "../../prisma.js";
 import { compare } from "bcryptjs";
 import pkg from "jsonwebtoken";
@@ -7,34 +8,47 @@ const { sign } = pkg;
 
 class AuthUserService {
   async execute({ email, password }) {
-    const user = await prismaClient.user.findUnique({
+    let user = await prismaClient.colaborador.findUnique({
       where: { email: email },
     });
 
     if (!user) {
-      throw new AppError("Usuário ou senha incorreto");
+      user = await prismaClient.user.findUnique({
+        where: { email: email },
+      });
+    }
+
+    if (!user) {
+      throw new AppError("Usuário ou senha incorreto", 401);
     }
 
     const passwordMatch = await compare(password, user.password);
-
     if (!passwordMatch) {
-      throw new AppError("Usuário ou senha incorreto");
+      throw new AppError("Usuário ou senha incorreto", 401);
     }
 
     const secret = process.env.JWT_SECRET;
     if (!secret) {
-      throw new AppError("Erro interno do servidor");
+      throw new AppError(
+        "Erro interno do servidor: Chave JWT não configurada",
+        500
+      );
     }
 
-    const token = sign({ name: user.name, email: user.email }, secret, {
-      subject: user.id,
-      expiresIn: "1d",
-    });
+    const token = sign(
+      { name: user.name, email: user.email, role: user.role },
+      secret,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
 
     return {
       id: user.id,
       name: user.name,
       email: user.email,
+      role: user.role,
       token: token,
     };
   }
