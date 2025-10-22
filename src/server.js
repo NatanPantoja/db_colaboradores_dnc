c332cba7eb05e56d54822b57e5c6195cb56c8302
Natanael Pantoja <161660708+NatanPantoja@users.noreply.github.com>
2025-10-22 08:36:29 -0300

    Define PORT constant for server configuration

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import router from "./router.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 const app = express();
+const PORT = process.env.PORT || 10000;
 const corsOptions = {
   origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
@@ -18,5 +19,5 @@ app.use(express.json());
 app.use(router);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 10000;
+
 app.listen(PORT, () => console.log(`Servidor Online ${PORT}`));
